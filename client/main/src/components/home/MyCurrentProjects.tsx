304583e51d0f24e45ebef03daa1f17e2511d66f9
Yiğit Levent <yigitlevent@live.com>
2023-01-15 15:11:04 +0300

    adds forgotten protocols

--- client/main/src/components/home/MyCurrentProjects.tsx
+++ client/main/src/components/home/MyCurrentProjects.tsx
@@ -9,7 +9,7 @@ export function MyCurrentProjects() {
 		<Fragment>
 			<Grid item xs={2} md={1}>
 				<Typography variant="h6">
-					<Link href="yigitlevent.com" sx={{ textDecoration: "none" }}>yigitlevent.com</Link>
+					<Link href="https://yigitlevent.com" sx={{ textDecoration: "none" }}>yigitlevent.com</Link>
 				</Typography>
 				<Typography>
 					This is my sysadmin, devops, backend, and frontend project. I could have done all of this with a hosting service and such, but I wanted to overcomplicate things to learn more about how to use a server, extend my linux skills, and learn more about devops.
@@ -17,7 +17,7 @@ export function MyCurrentProjects() {
 			</Grid>
 			<Grid item xs={2} md={1}>
 				<Typography variant="h6">
-					<Link href="bwgrtools.yigitlevent.com" sx={{ textDecoration: "none" }}>BWGR Tools</Link>
+					<Link href="https://bwgrtools.yigitlevent.com" sx={{ textDecoration: "none" }}>BWGR Tools</Link>
 				</Typography>
 				<Typography variant="body1">
 					New iteration of one of my oldest projects, this one is a set of utilities for a tabletop game called Burning Wheel Gold. Plan is to integrate this with this api to provide a better user experience.
