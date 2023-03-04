3e6f846a0411ac73a7e4f99668495ea1e7dd05ff
Yiğit Levent <yigitlevent@live.com>
2023-03-04 03:16:09 +0300

    changes to the data converter

--- sql/converter/src/index.ts
+++ sql/converter/src/index.ts
@@ -2,13 +2,16 @@ import fs from "fs";
 
 import { processRulesets } from "./groups/g1_rulesets.js";
 
-
+// PROCESS OLD DATA
 const rulesets = processRulesets();
 
+// ADD AN ARRAY OF SQL STRINGS
 const outputs = [
 	...rulesets
 ];
 
-fs.rmSync("./output", { recursive: true });
-fs.mkdirSync("./output");
-fs.writeFileSync("./output/data.sql", outputs.map(v => `${v};`).join("\n\n"));
+// WRITE DATA INTO A FILE
+const outputPath = "../data";
+if (fs.existsSync(outputPath)) fs.rmSync(outputPath, { recursive: true });
+fs.mkdirSync(outputPath);
+fs.writeFileSync(`${outputPath}/data.sql`, outputs.map(v => `${v};`).join("\n\n") + "\n");
