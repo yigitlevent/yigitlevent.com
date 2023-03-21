ced51300288842f459ddd34ca0e5308ef6940b11
Yiğit Levent <yigitlevent@live.com>
2023-03-21 13:43:04 +0300

    moves tests object

--- client/bwgrtools/src/components/Tools/DiceRoller/DiceRoller.tsx
+++ client/bwgrtools/src/components/Tools/DiceRoller/DiceRoller.tsx
@@ -17,11 +17,33 @@ import Looks4Icon from "@mui/icons-material/Looks4";
 import Looks5Icon from "@mui/icons-material/Looks5";
 import Looks6Icon from "@mui/icons-material/Looks6";
 
-import { Tests } from "../../../data/tests";
 import { RandomNumber } from "../../../utils/misc";
 
 import { GenericGrid } from "../../Shared/Grids";
 
+interface Tests {
+	[key: number]: { routineMaxObstacle: number; };
+}
+
+const Tests: Tests = {
+	2: { routineMaxObstacle: 1 },
+	3: { routineMaxObstacle: 2 },
+	4: { routineMaxObstacle: 2 },
+	5: { routineMaxObstacle: 3 },
+	6: { routineMaxObstacle: 4 },
+	7: { routineMaxObstacle: 4 },
+	8: { routineMaxObstacle: 5 },
+	9: { routineMaxObstacle: 6 },
+	10: { routineMaxObstacle: 7 },
+	11: { routineMaxObstacle: 8 },
+	12: { routineMaxObstacle: 9 },
+	13: { routineMaxObstacle: 10 },
+	14: { routineMaxObstacle: 11 },
+	15: { routineMaxObstacle: 12 },
+	16: { routineMaxObstacle: 13 },
+	17: { routineMaxObstacle: 14 },
+	18: { routineMaxObstacle: 15 }
+};
 
 interface TestResult {
 	dice: number[];
