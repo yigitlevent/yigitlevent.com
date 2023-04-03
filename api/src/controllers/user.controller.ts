00667064f621cb1098225d88088ecb1e314f8e23
Yiğit Levent <yigitlevent@live.com>
2023-04-03 20:41:12 +0300

    improves validation/sanitization, simplifies a couple of implementations

--- api/src/controllers/user.controller.ts
+++ api/src/controllers/user.controller.ts
@@ -2,7 +2,7 @@ import { Request, Response } from "express";
 import bcrypt from "bcrypt";
 
 import { PgPool } from "../index";
-
+import { FindUserByEmail, UpdateUserLastSignInAt } from "../services/user.service";
 
 
 export async function UserAuth(request: Request, response: Response) {
@@ -12,21 +12,15 @@ export async function UserAuth(request: Request, response: Response) {
 export async function UserSignUp(request: Request, response: Response) {
 	const { username, email, password } = request.body;
 
-	if (username == null || email == null || password == null) return response.sendStatus(403);
-
 	try {
-		const hashedPassword = bcrypt.hashSync(request.body.password, 10);
+		const hashedPassword = bcrypt.hashSync(password, 10);
 
-		const query = `insert into usr."Users"("Username", "Email", "Password") values ('${username}', '${email}', '${hashedPassword}') returning *;`;
-
-		const data = await PgPool.query(query);
-
-		if (data.rows.length === 0) { response.sendStatus(403); }
+		const data = await PgPool.query<UserDBO>(`insert into usr."Users"("Username", "Email", "Password") values ('${username}', '${email}', '${hashedPassword}') returning *;`);
+		if (data.rows.length === 0) throw new Error("unable to sign user up");
 
 		const user = data.rows[0];
 
-		request.session.user = { id: user.Id, username: user.username, email: user.email };
-
+		request.session.user = { id: user.Id, username: user.Username, email: user.Email };
 		response.status(200);
 		return response.json({ user: request.session.user });
 	}
@@ -39,30 +33,21 @@ export async function UserSignUp(request: Request, response: Response) {
 export async function UserSignIn(request: Request, response: Response) {
 	const { email, password } = request.body;
 
-	if (email == null || password == null) { return response.sendStatus(403); }
-
 	try {
-		const query = `select "Id", "Username", "Email", "Password" from usr."Users" where "Email" = '${email}';`;
+		const user = await FindUserByEmail(email);
 
-		const data = await PgPool.query<UserDBO>(query);
+		if (user) {
+			const matches = bcrypt.compareSync(password, user.Password);
+			if (!matches) throw new Error("invalid password");
 
-		if (data.rows.length === 0) { return response.sendStatus(403); }
-		const user = data.rows[0];
+			UpdateUserLastSignInAt(user.Id);
 
-		const matches = bcrypt.compareSync(password, user.Password);
-		if (!matches) { return response.sendStatus(403); }
+			request.session.user = { id: user.Id, username: user.Username, email: user.Email };
+			response.status(200);
+			return response.json({ user: request.session.user });
+		}
 
-		request.session.user = { id: user.Id, username: user.Username, email: user.Email };
-
-		const updateQuery =
-			`UPDATE usr."Users" 
-				SET "LastSigninAt" = (to_timestamp(${Date.now()} / 1000.0))
-				WHERE "Id" = '${user.Id}';`;
-
-		PgPool.query(updateQuery);
-
-		response.status(200);
-		return response.json({ user: request.session.user });
+		throw new Error("username not found");
 	}
 	catch (e) {
 		console.error(e);
--- api/src/services/user.service.ts
+++ api/src/services/user.service.ts
@@ -4,17 +4,20 @@ import { PgPool } from "../index";
 export async function FindUserByEmail(email: string) {
 	const query = `select * from usr."Users" where "Email" = '${email}';`;
 
-	const data = await PgPool.query(query);
+	const data = await PgPool.query<UserDBO>(query);
 
 	if (data.rows.length > 0) return data.rows[0];
-	else return false;
 }
 
 export async function FindUserByUsername(username: string) {
 	const query = `select * from usr."Users" where "Username" = '${username}';`;
 
-	const data = await PgPool.query(query);
+	const data = await PgPool.query<UserDBO>(query);
 
 	if (data.rows.length > 0) return data.rows[0];
 	else return false;
+}
+
+export async function UpdateUserLastSignInAt(userId: string) {
+	PgPool.query(`update usr."Users" set "LastSigninAt" = (to_timestamp(${Date.now()} / 1000.0)) where "Id" = '${userId}';`);
 }
