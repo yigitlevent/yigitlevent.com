aa2c14f2dc308a692eeffac7d4340f6ec8845f93
Yiğit Levent <yigitlevent@live.com>
2023-11-10 00:01:58 +0300

    adds typescript to base

--- @types/utility/nominal.d.ts
+++ @types/utility/nominal.d.ts
@@ -5,6 +5,6 @@ interface NominalNumber<T extends number> extends Number {
 	readonly __typeName: T;
 }*/
 
-declare const NOMINAL_BRAND: unique symbol
+declare const NOMINAL_BRAND: unique symbol;
 
-type Nominal<T> = T & { [NOMINAL_BRAND]: never };
+type Nominal<T> = T & { [NOMINAL_BRAND]: T; };
