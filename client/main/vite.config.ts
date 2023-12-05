3c3856f4ebd74e7d90e0335c7666572c962e41ff
Yiğit Levent <yigitlevent@live.com>
2023-12-05 19:34:03 +0300

    adds manual chunks to builds

--- client/main/vite.config.ts
+++ client/main/vite.config.ts
@@ -3,5 +3,14 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()]
+	plugins: [react()],
+	build: {
+		rollupOptions: {
+			output: {
+				manualChunks(id) {
+					if (id.includes('node_modules')) return "modules";
+				}
+			}
+		}
+	}
 });
