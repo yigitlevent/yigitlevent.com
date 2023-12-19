0608e190b39bf34749a3c3dc9c2e674b4d36fe86
Yiğit Levent <yigitlevent@live.com>
2023-12-19 17:58:45 +0300

    updates cors options

--- api/src/configs/cors.config.ts
+++ api/src/configs/cors.config.ts
@@ -6,6 +6,6 @@ import { IsDev } from "./constants.config";
 export const CorsConfig: CorsOptions = {
 	origin: IsDev ? "*" : ["https://yigitlevent.com", /\.yigitlevent\.com$/],
 	methods: ["POST", "GET", "PUT", "DELETE", "OPTIONS", "HEAD"],
-	credentials: false
+	credentials: IsDev ? false : true
 	//allowedHeaders: ["Content-Type", "Authorization"]
 };
