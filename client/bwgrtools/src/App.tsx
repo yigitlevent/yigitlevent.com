09306a34efd89f575cb47ccb2b29834e23f510b2
Yiğit Levent <yigitlevent@live.com>
2024-01-16 14:02:14 +0300

    fixes build issue

--- client/bwgrtools/src/App.tsx
+++ client/bwgrtools/src/App.tsx
@@ -1,3 +1,4 @@
+import { Average } from "@utility/Average";
 import { useEffect } from "react";
 
 import { MainBox } from "./components/MainBox";
@@ -6,6 +7,8 @@ import { useUserStore } from "./hooks/apiStores/useUserStore";
 
 export function App(): JSX.Element {
 	const { triedAuth, auth } = useUserStore();
+
+	Average([1, 2, 3]);
 
 	useEffect(() => {
 		if (!triedAuth) { auth(); }
