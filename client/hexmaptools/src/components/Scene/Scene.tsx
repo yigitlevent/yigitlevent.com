5c6357f12f001fcc44c73a4ca792e8df2744495a
Yiğit Levent <yigitlevent@live.com>
2024-01-27 21:26:40 +0300

    changes stage render order

--- client/hexmaptools/src/components/Scene/Scene.tsx
+++ client/hexmaptools/src/components/Scene/Scene.tsx
@@ -1,15 +1,20 @@
 import { Stage, Container } from "@pixi/react";
 
-import { Areas } from "./Scene/Areas";
-import { Hexes } from "./Scene/Hexes";
+import { AreaTerrain } from "./Scene/Areas/AreaTerrain";
+import { HexBiome } from "./Scene/Hexes/HexBiome";
+import { HexTerrain } from "./Scene/Hexes/HexTerrain";
 import { Names } from "./Scene/Names";
-import { Shapes } from "./Scene/Shapes";
+import { AreaShape } from "./Scene/Shapes/AreaShape";
+import { HexShape } from "./Scene/Shapes/HexShape";
 import { Viewport } from "./Viewport";
 import { useHexmapStore } from "../../hooks/apiStores/useHexmapStore";
 
 
 export function Scene({ height, width }: { height: number; width: number; }): JSX.Element {
-	const map = useHexmapStore(state => state.map);
+	const [map, hexes, areas] = useHexmapStore(state => [state.map, state.hexes, state.areas]);
+
+	const validAreas = Array.from(areas);
+	const validHexes = Array.from(hexes);
 
 	return (
 		<Stage
@@ -21,9 +26,11 @@ export function Scene({ height, width }: { height: number; width: number; }): JS
 			<Viewport>
 				{map.id.length > -1
 					? <Container position={[width / 2, height / 2]}>
-						<Hexes />
-						<Areas />
-						<Shapes />
+						{validHexes.map((keyValue, index) => <HexBiome key={index} hex={keyValue[1]} />)}
+						{validAreas.map((keyValue, index) => <AreaTerrain key={index} area={keyValue[1]} />)}
+						{validHexes.map((keyValue, index) => <HexTerrain key={index} hex={keyValue[1]} />)}
+						{validAreas.map((keyValue, index) => <AreaShape key={index} area={keyValue[1]} />)}
+						{validHexes.map((keyValue, index) => <HexShape key={index} hex={keyValue[1]} />)}
 						<Names />
 					</Container>
 					: null}
