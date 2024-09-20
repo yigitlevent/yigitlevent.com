946ad757ef7a692e2f3e819f7be4036c2241f42d
Yiğit Levent <yigitlevent@live.com>
2024-09-20 04:13:53 +0300

    removes hexmaptools

--- client/hexmaptools/src/utils/TextureKeyResolver.ts
+++
@@ -1,4 +0,0 @@
-//export function TextureKeyResolver(textureName: string, terrainType: HmTerrainExactType): HmTextureName {
-//	if (textureName.includes("nothing")) return "nothing";
-//	return `${textureName}_${terrainType.toLocaleLowerCase()}`;
-//}
