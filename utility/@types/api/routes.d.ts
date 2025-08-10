38e8a4b3ec8d8a4a7bd6f8220517256b50e32ffb
Yiğit Levent <yigitlevent@live.com>
2025-08-10 09:50:46 +0300

    adds missing route

--- utility/@types/api/routes.d.ts
+++ utility/@types/api/routes.d.ts
@@ -1,4 +1,5 @@
 type UserRoutes = `/user${"/auth" | "/signin" | "/signup" | "/signout"}`;
 type BwgrRulesetRoutes = `/bwgr/ruleset/${"list" | "data"}`;
+type MggmRulesetRoutes = "/mggm/megagame";
 
-type Routes = UserRoutes | BwgrRulesetRoutes;
+type Routes = UserRoutes | BwgrRulesetRoutes | MggmRulesetRoutes;
