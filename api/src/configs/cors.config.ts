1041979ae1163795c9c4dc16c4d7c1b96c262297
Yiğit Levent <yigitlevent@live.com>
2026-04-07 22:58:16 +0300

    i hate this

--- api/src/configs/cors.config.ts
+++ api/src/configs/cors.config.ts
@@ -6,6 +6,6 @@ import type { CorsOptions } from "cors";
 export const CorsConfig: CorsOptions = {
 	origin: IsDev ? "*" : ["https://yigitlevent.com", "https://api.yigitlevent.com", "https://bwgrtools.yigitlevent.com", "https://megagame.yigitlevent.com"],
 	methods: ["POST", "GET", "PUT", "DELETE", "OPTIONS", "HEAD"],
-	credentials: true
-	// allowedHeaders: ["Content-Type", "Authorization"]
+	credentials: true,
+	allowedHeaders: "*"
 };
